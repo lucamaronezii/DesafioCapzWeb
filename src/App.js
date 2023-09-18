2c8abd6bbd0439628c49d5c30871270e3b14765b
Luca <steyerluca@hotmail.com>
2023-09-18 11:48:15 -0300

    Desenvolvimento Front

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Importe os estilos Bootstrap
+
+function formatarResultados(resultados) {
+  // Função para formatar os resultados
+  return resultados.map((resultado) => {
+    const formattedResultado = {};
+    for (const key in resultado) {
+      if (resultado.hasOwnProperty(key)) {
+        formattedResultado[`${key.charAt(0).toUpperCase() + key.slice(1)}:`] = resultado[key];
+      }
+    }
+    return formattedResultado;
+  });
+}
 
 function App() {
+  const [palavraChave, setPalavraChave] = useState('');
+  const [resultados, setResultados] = useState([]);
+  const [erro, setErro] = useState(null);
+  const [modoEscuro, setModoEscuro] = useState(false); // Estado para controlar o modo escuro
+
+  const handleInputChange = async (e) => {
+    const valor = e.target.value;
+    setPalavraChave(valor);
+
+    try {
+      const response = await fetch(`https://localhost:7082/Sistema/pesquisar?palavraChave=${valor}`);
+      const data = await response.json();
+      const formattedData = formatarResultados(data); // Formate os resultados
+      setResultados(formattedData);
+      setErro(null);
+    } catch (error) {
+      console.error('Erro ao buscar resultados:', error);
+      setResultados([]);
+      setErro('Erro ao buscar resultados. Verifique a conexão com a API.');
+    }
+  };
+
+  const toggleModo = () => {
+    setModoEscuro(!modoEscuro); // Inverte o estado do modo
+  };
+
+  useEffect(() => {
+    // Aplica as classes Bootstrap com base no modo selecionado
+    if (modoEscuro) {
+      document.body.classList.add('bg-dark', 'text-light');
+    } else {
+      document.body.classList.remove('bg-dark', 'text-light');
+    }
+  }, [modoEscuro]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <div className="d-flex justify-content-between align-items-center mb-3">
+        <h1 className={`text-center ${modoEscuro ? 'text-light' : ''}`}>Multisearch</h1>
+        <button className={`btn ${modoEscuro ? 'btn-light' : 'btn-dark'}`} onClick={toggleModo}>
+          {modoEscuro ? 'Modo Claro' : 'Modo Escuro'}
+        </button>
+      </div>
+      <div className="input-group mb-3">
+        <input
+          type="text"
+          className={`form-control ${modoEscuro ? 'bg-dark text-light' : ''}`}
+          placeholder="Digite sua pesquisa e pressione Enter"
+          value={palavraChave}
+          onChange={handleInputChange}
+        />
+      </div>
+      {erro && <p className="text-danger">{erro}</p>}
+      <ul className={`list-group ${modoEscuro ? 'bg-dark text-light' : ''}`}>
+        {resultados.length === 0 ? (
+          <li className="list-group-item">Nenhum objeto encontrado.</li>
+        ) : (
+          resultados.map((resultado, index) => (
+            <li className={`list-group-item ${modoEscuro ? 'bg-dark text-light' : ''}`} key={index}>
+              {Object.entries(resultado).map(([key, value]) => (
+                <p key={key}>
+                  <strong>{key}</strong> {value}
+                </p>
+              ))}
+            </li>
+          ))
+        )}
+      </ul>
     </div>
   );
 }
